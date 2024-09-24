a7d78696d24732498617e68f68fd5e12f28bf473
Ritik Raghav <ritikrghv313@gmail.com>
2024-09-24 18:13:49 +0530

    Dynamic Routes Part 3 (Query Params)

--- models/cart.js
+++ models/cart.js
@@ -34,11 +34,41 @@ module.exports = class Cart {
                 updatedProduct = {id: id, qty: 1};
                 cart.products = [...cart.products, updatedProduct];
             }
-            cart.totalPrice = cart.totalPrice + productPrice;
+            cart.totalPrice = cart.totalPrice + +productPrice;
             fs.writeFile(p, JSON.stringify(cart), (error) => {
                 console.log(error);
-            })
+            });
+        });
+    };
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (error, fileContent) => {
+            if (error) {
+                return;
+            }
+            const updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod => prod.id === id);
+            if (!product) {
+                return;
+            }
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+            fs.writeFile(p, JSON.stringify(updatedCart), (error) => {
+                console.log(error);
+            });
         })
-        
+    }
+
+    static getCart(cb) {
+        fs.readFile(p, (error, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if (error) {
+                cb(null);
+            }
+            else {
+                cb(cart);
+            }
+        })
     }
 }
